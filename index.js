ee88f6da4b03db407ff716c9528039bdece22e7b
areumm <areumm914@gmail.com>
2023-05-13 00:38:14 +0900

    bodyparser , postman 데이터 확인

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+import bodyParser from "body-parser";
+
 import productRoute from "./router/product.js"; // produect.js 만든다음에 여기에 import 꼭 생성
 
 
@@ -10,7 +12,10 @@ const app = express();
 app.use(cors()); //외부접근 라이브러리
 app.use(morgan("dev")); //미들웨어 설정 (로그 작성)
 
-app.use("/product", productRoute);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}))
+
+app.use("/product", productRoute); // post 맨 product 경로
 
 const port = 8080;
 app.get("/test", (req,res) => {
--- router/product.js
+++ router/product.js
@@ -3,4 +3,36 @@ const router = express.Router();
 
 console.log("시작!!");
 
+// CRUD 생성
+router.get("/all", (req,res) => {
+    res.json({
+        msg : "product get all"
+    })
+})
+
+router.post("/create", (req,res) => {
+   const userInput = {
+       name : req.body.produtName,
+       price : req.body.productPrice
+   }
+
+    res.json({
+        msg : "create & product",
+        product : userInput
+    })
+})
+
+router.put("/update", (req,res) => {
+    res.json({
+        msg : "update & product"
+    })
+})
+
+router.delete("/delete", (req, res) => {
+    res.json({
+        msg : "delete & product"
+    })
+})
+
+
 export default router;
